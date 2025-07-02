eb913fe3ab18eab0c43a8297d312348f82d146f0
Aryanjast13 <aryanjastaj05@gmail.com>
2025-07-02 20:09:50 +0530

    ENV SERVER URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE==="development"?"http://localhost:3800/api/v1":"/api/v1",
+  baseURL: import.meta.env.MODE==="development"?import.meta.VITE_SERVER_URL:"/api/v1",
   headers: {
     'Content-Type': 'application/json',
   },
